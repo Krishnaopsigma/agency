07f57d9593c9810358354d4aafe1c66bbd01556f
Krishnaopsigma <krishnavelmurugan10@gmail.com>
2024-12-08 11:19:33 +0000

    Add files via upload

---
+++ callculator.py
@@ -0,0 +1,34 @@
+
+
+def add(a,b):
+    return a+b
+
+def mul(a,b):
+    return a*b
+
+
+def div(a,b):
+    return a/b
+
+def sub(a,b):
+    return a-b
+
+print("Basic Calculator")
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
+choice=int(input("enter the choice"))
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+
+if choice==1:
+    print(add(a,b))
+elif choice==2:
+    print(sub(a,b))
+elif choice==3:
+    print(mul(a,b))
+elif choice==4:
+    print(div(a,b))
+else:
+    print("Enter a valid choice!!!")
